ec2edc95c4d8cf072e97824da2c88677ac66e2e7
MirageFairy <idolm@ster>
2023-11-21 04:31:41 +0900

    build: add: template

---
+++ src/client/kotlin/dummy12345678/DUMMY12345678Client.kt
@@ -0,0 +1,9 @@
+package dummy12345678
+
+import net.fabricmc.api.ClientModInitializer
+
+object DUMMY12345678Client : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/dummy12345678/DUMMY12345678DataGenerator.kt
@@ -0,0 +1,9 @@
+package dummy12345678
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object DUMMY12345678DataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
