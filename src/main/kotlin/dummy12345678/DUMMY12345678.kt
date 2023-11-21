6644d634de790a72636f057191ed84bbc24719bf
MirageFairy <idolm@ster>
2023-11-21 22:58:29 +0900

    ADD: fairy_plastic // creative only

--- src/main/kotlin/dummy12345678/DUMMY12345678.kt
+++ src/main/kotlin/dummy12345678/DUMMY12345678.kt
@@ -1,15 +1,27 @@
 package dummy12345678
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import org.slf4j.LoggerFactory
 
 object DUMMY12345678 : ModInitializer {
     private val logger = LoggerFactory.getLogger("dummy12345678")
+
+    val fairyPlasticItem = Item(Item.Settings())
 
     override fun onInitialize() {
         // This code runs as soon as Minecraft is in a mod-load-ready state.
         // However, some things (like resources) may still be uninitialized.
         // Proceed with mild caution.
         logger.info("Hello Fabric world!")
+        Registry.register(Registries.ITEM, Identifier("dummy12345678", "fairy_plastic"), fairyPlasticItem)
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register {
+            it.add(fairyPlasticItem)
+        }
     }
 }
--- src/main/kotlin/dummy12345678/DUMMY12345678DataGenerator.kt
+++ src/main/kotlin/dummy12345678/DUMMY12345678DataGenerator.kt
@@ -2,8 +2,40 @@ package dummy12345678
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider
+import net.minecraft.data.client.BlockStateModelGenerator
+import net.minecraft.data.client.ItemModelGenerator
+import net.minecraft.data.client.Models
 
 object DUMMY12345678DataGenerator : DataGeneratorEntrypoint {
     override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+        val pack = fabricDataGenerator.createPack()
+        pack.addProvider { output: FabricDataOutput ->
+            object : FabricModelProvider(output) {
+                override fun generateBlockStateModels(blockStateModelGenerator: BlockStateModelGenerator) {
+
+                }
+
+                override fun generateItemModels(itemModelGenerator: ItemModelGenerator) {
+                    itemModelGenerator.register(DUMMY12345678.fairyPlasticItem, Models.GENERATED)
+                }
+            }
+        }
+        pack.addProvider { output: FabricDataOutput ->
+            object : FabricLanguageProvider(output, "en_us") {
+                override fun generateTranslations(translationBuilder: TranslationBuilder) {
+                    translationBuilder.add(DUMMY12345678.fairyPlasticItem, "Fairy Plastic")
+                }
+            }
+        }
+        pack.addProvider { output: FabricDataOutput ->
+            object : FabricLanguageProvider(output, "ja_jp") {
+                override fun generateTranslations(translationBuilder: TranslationBuilder) {
+                    translationBuilder.add(DUMMY12345678.fairyPlasticItem, "妖精のプラスチック")
+                }
+            }
+        }
     }
 }
