3cb2aa12db6bbea3969c4b6ae37b633459d7570b
MirageFairy <idolm@ster>
2023-11-22 03:37:38 +0900

    refactor: MirageFairy2024

--- src/main/kotlin/miragefairy2024/MirageFairy2024.kt
+++ src/main/kotlin/miragefairy2024/MirageFairy2024.kt
@@ -6,15 +6,9 @@ import org.slf4j.LoggerFactory
 
 object MirageFairy2024 : ModInitializer {
     val modId = "miragefairy2024"
-    private val logger = LoggerFactory.getLogger("miragefairy2024")
+    val logger = LoggerFactory.getLogger("miragefairy2024")
 
     override fun onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
-        logger.info("Hello Fabric world!")
-
         initMaterialsModule()
-
     }
 }
