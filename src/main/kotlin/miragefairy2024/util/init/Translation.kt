686425c3d05c95ff4df26ed2c025df5d4c0dc0f4
MirageFairy <idolm@ster>
2023-11-22 05:44:58 +0900

    internal: add: en, ja

--- src/main/kotlin/miragefairy2024/util/init/Translation.kt
+++ src/main/kotlin/miragefairy2024/util/init/Translation.kt
@@ -3,6 +3,20 @@ package miragefairy2024.util.init
 import miragefairy2024.MirageFairy2024DataGenerator
 import net.minecraft.item.Item
 
+fun en(getter: () -> Pair<String, String>) {
+    MirageFairy2024DataGenerator.englishTranslations += {
+        val pair = getter()
+        it.add(pair.first, pair.second)
+    }
+}
+
+fun ja(getter: () -> Pair<String, String>) {
+    MirageFairy2024DataGenerator.japaneseTranslations += {
+        val pair = getter()
+        it.add(pair.first, pair.second)
+    }
+}
+
 fun Item.en(enName: String) {
     MirageFairy2024DataGenerator.englishTranslations += {
         it.add(this, enName)
