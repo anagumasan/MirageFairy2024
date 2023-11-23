9702d32afcf52db4f174ff1ce3280523a3f30117
MirageFairy <idolm@ster>
2023-11-24 03:46:29 +0900

    internal: add: identifier

--- src/main/kotlin/miragefairy2024/util/Identifier.kt
+++ src/main/kotlin/miragefairy2024/util/Identifier.kt
@@ -1,8 +1,14 @@
 package miragefairy2024.util
 
+import net.minecraft.block.Block
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
 
 val Identifier.string get() = this.toString()
 
 infix fun String.concat(identifier: Identifier) = Identifier(identifier.namespace, this + identifier.path)
 infix fun Identifier.concat(string: String) = Identifier(this.namespace, this.path + string)
+
+val Block.identifier get() = Registries.BLOCK.getId(this)
+val Item.identifier get() = Registries.ITEM.getId(this)
