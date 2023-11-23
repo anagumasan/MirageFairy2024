dfe090a3295753daee4a6e9bd6bc29a70e6483d4
MirageFairy <idolm@ster>
2023-11-24 03:47:15 +0900

    internal: Model: add

--- src/main/kotlin/miragefairy2024/util/init/Model.kt
+++ src/main/kotlin/miragefairy2024/util/init/Model.kt
@@ -1,9 +1,17 @@
 package miragefairy2024.util.init
 
 import miragefairy2024.MirageFairy2024DataGenerator
+import miragefairy2024.util.concat
+import miragefairy2024.util.identifier
+import miragefairy2024.util.with
+import net.minecraft.block.Block
+import net.minecraft.data.client.BlockStateModelGenerator
 import net.minecraft.data.client.Model
 import net.minecraft.data.client.Models
+import net.minecraft.data.client.TextureKey
+import net.minecraft.data.client.TexturedModel
 import net.minecraft.item.Item
+import net.minecraft.util.Identifier
 
 fun Item.registerItemModelGeneration(model: Model) {
     MirageFairy2024DataGenerator.itemModelGenerations += {
@@ -12,3 +20,18 @@ fun Item.registerItemModelGeneration(model: Model) {
 }
 
 fun Item.registerGeneratedItemModelGeneration() = this.registerItemModelGeneration(Models.GENERATED)
+
+fun TexturedModel.registerModelGeneration(identifier: Identifier) {
+    MirageFairy2024DataGenerator.blockStateModelGenerations += {
+        this.model.upload(identifier, this.textures, it.modelCollector)
+    }
+}
+
+fun Model.registerModelGeneration(identifier: Identifier, vararg textureEntries: Pair<TextureKey, Identifier>) = this.with(*textureEntries).registerModelGeneration(identifier)
+
+
+fun Block.registerSingletonBlockStateGeneration() {
+    MirageFairy2024DataGenerator.blockStateModelGenerations += {
+        it.blockStateCollector.accept(BlockStateModelGenerator.createSingletonBlockState(this, "block/" concat this.identifier))
+    }
+}
