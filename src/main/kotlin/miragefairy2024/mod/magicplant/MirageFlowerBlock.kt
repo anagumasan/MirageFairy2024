c7ae7f68ee26622e0283b5cbf79d4f1e1f42cf47
MirageFairy <idolm@ster>
2023-11-25 11:36:08 +0900

    VIEW: mirage_flower: カーソルの当たり判定を調整、地面を設定

--- src/main/kotlin/miragefairy2024/mod/magicplant/MirageFlowerBlock.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/MirageFlowerBlock.kt
@@ -1,17 +1,35 @@
 package miragefairy2024.mod.magicplant
 
+import mirrg.kotlin.hydrogen.atLeast
+import mirrg.kotlin.hydrogen.atMost
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
+import net.minecraft.block.Blocks
+import net.minecraft.block.ShapeContext
+import net.minecraft.block.SideShapeType
 import net.minecraft.state.StateManager
 import net.minecraft.state.property.IntProperty
 import net.minecraft.state.property.Properties
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
+import net.minecraft.util.shape.VoxelShape
+import net.minecraft.world.BlockView
 
 @Suppress("OVERRIDE_DEPRECATION")
 class MirageFlowerBlock(settings: Settings) : MagicPlantBlock(settings) {
     companion object {
         val AGE: IntProperty = Properties.AGE_3
         const val MAX_AGE = 3
+        private val AGE_TO_SHAPE: Array<VoxelShape> = arrayOf(
+            createCuboidShape(5.0, 0.0, 5.0, 11.0, 5.0, 11.0),
+            createCuboidShape(2.0, 0.0, 2.0, 14.0, 12.0, 14.0),
+            createCuboidShape(2.0, 0.0, 2.0, 14.0, 15.0, 14.0),
+            createCuboidShape(2.0, 0.0, 2.0, 14.0, 16.0, 14.0),
+        )
     }
+
+
+    // Property
 
     init {
         defaultState = defaultState.with(AGE, 0)
@@ -20,4 +38,15 @@ class MirageFlowerBlock(settings: Settings) : MagicPlantBlock(settings) {
     override fun appendProperties(builder: StateManager.Builder<Block, BlockState>) {
         builder.add(AGE)
     }
+
+    fun getAge(state: BlockState) = state[AGE]!!
+    fun isMaxAge(state: BlockState) = getAge(state) >= MAX_AGE
+    fun withAge(age: Int): BlockState = defaultState.with(AGE, age atLeast 0 atMost MAX_AGE)
+
+
+    // Shape
+
+    override fun getOutlineShape(state: BlockState, world: BlockView, pos: BlockPos, context: ShapeContext) = AGE_TO_SHAPE[getAge(state)]
+    override fun canPlantOnTop(floor: BlockState, world: BlockView, pos: BlockPos) = world.getBlockState(pos).isSideSolid(world, pos, Direction.UP, SideShapeType.CENTER) || floor.isOf(Blocks.FARMLAND)
+
 }
