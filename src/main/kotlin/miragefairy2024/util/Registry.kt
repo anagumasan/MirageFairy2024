4d35378efb3935f8629709456b02d2d60ed772dd
MirageFairy <idolm@ster>
2023-11-25 23:49:55 +0900

    internal: add: BlockEntityType.register

--- src/main/kotlin/miragefairy2024/util/Registry.kt
+++ src/main/kotlin/miragefairy2024/util/Registry.kt
@@ -2,6 +2,7 @@ package miragefairy2024.util
 
 import mirrg.kotlin.hydrogen.unit
 import net.minecraft.block.Block
+import net.minecraft.block.entity.BlockEntityType
 import net.minecraft.item.Item
 import net.minecraft.item.ItemGroup
 import net.minecraft.registry.Registries
@@ -9,6 +10,8 @@ import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 fun Block.register(identifier: Identifier) = unit { Registry.register(Registries.BLOCK, identifier, this) }
+
+fun BlockEntityType<*>.register(identifier: Identifier) = unit { Registry.register(Registries.BLOCK_ENTITY_TYPE, identifier, this) }
 
 fun Item.register(identifier: Identifier) = unit { Registry.register(Registries.ITEM, identifier, this) }
 
