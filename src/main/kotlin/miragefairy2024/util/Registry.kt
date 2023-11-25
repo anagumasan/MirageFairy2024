57b1a807e6a5be3dba6aa73b81e373c7fe82b26d
MirageFairy <idolm@ster>
2023-11-25 10:42:37 +0900

    internal: add: ItemGroup.register

--- src/main/kotlin/miragefairy2024/util/Registry.kt
+++ src/main/kotlin/miragefairy2024/util/Registry.kt
@@ -3,6 +3,7 @@ package miragefairy2024.util
 import mirrg.kotlin.hydrogen.unit
 import net.minecraft.block.Block
 import net.minecraft.item.Item
+import net.minecraft.item.ItemGroup
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
@@ -10,3 +11,5 @@ import net.minecraft.util.Identifier
 fun Block.registerBlock(identifier: Identifier) = unit { Registry.register(Registries.BLOCK, identifier, this) }
 
 fun Item.registerItem(identifier: Identifier) = unit { Registry.register(Registries.ITEM, identifier, this) }
+
+fun ItemGroup.register(identifier: Identifier) = unit { Registry.register(Registries.ITEM_GROUP, identifier, this) }
