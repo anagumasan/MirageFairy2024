0ce6b467e86ecd191e37c45f3c114f94d1820acf
MirageFairy <idolm@ster>
2023-12-01 04:24:42 +0900

    internal: add: EMPTY_ITEM_STACK

--- src/main/kotlin/miragefairy2024/util/ItemStack.kt
+++ src/main/kotlin/miragefairy2024/util/ItemStack.kt
@@ -4,3 +4,5 @@ import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 
 fun Item.createItemStack(count: Int = 1) = ItemStack(this, count)
+
+val EMPTY_ITEM_STACK: ItemStack get() = ItemStack.EMPTY
