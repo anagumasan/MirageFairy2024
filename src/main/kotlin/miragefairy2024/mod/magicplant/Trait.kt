7a7502fc3693b6e993252bffa8d966fe3a6180a6
MirageFairy <idolm@ster>
2023-12-01 07:22:10 +0900

    internal: add: Trait, TraitStack, TraitStacks, traitRegistry

---
+++ src/main/kotlin/miragefairy2024/mod/magicplant/Trait.kt
@@ -0,0 +1,29 @@
+package miragefairy2024.mod.magicplant
+
+import miragefairy2024.MirageFairy2024
+import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
+import net.fabricmc.fabric.api.event.registry.RegistryAttribute
+import net.minecraft.registry.Registry
+import net.minecraft.registry.RegistryKey
+import net.minecraft.util.Identifier
+import java.util.SortedMap
+
+// TraitRegistry
+
+val traitRegistryKey: RegistryKey<Registry<Trait>> = RegistryKey.ofRegistry(Identifier(MirageFairy2024.modId, "trait"))
+val traitRegistry: Registry<Trait> = FabricRegistryBuilder.createSimple(traitRegistryKey).attribute(RegistryAttribute.SYNCED).buildAndRegister()
+
+
+// Trait
+
+class Trait
+
+
+// TraitStack
+
+class TraitStack(val trait: Trait, val level: Int)
+
+
+// TraitStacks
+
+class TraitStacks private constructor(val traitStackMap: SortedMap<Trait, Int>)
