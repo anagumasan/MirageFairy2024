d5a5d7304ef31479101bebe9f86ef8c15b45d958
MirageFairy <idolm@ster>
2023-12-01 06:47:52 +0900

    internal: add: getClientPlayer

--- src/client/kotlin/miragefairy2024/MirageFairy2024Client.kt
+++ src/client/kotlin/miragefairy2024/MirageFairy2024Client.kt
@@ -4,7 +4,9 @@ import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback
 import net.minecraft.block.Block
+import net.minecraft.client.MinecraftClient
 import net.minecraft.client.render.RenderLayer
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.text.Text
 
@@ -20,6 +22,8 @@ object MirageFairy2024Client : ClientModInitializer {
             override fun registerCutoutRenderLayer(block: Block) {
                 BlockRenderLayerMap.INSTANCE.putBlock(block, RenderLayer.getCutout())
             }
+
+            override fun getClientPlayer(): PlayerEntity? = MinecraftClient.getInstance().player
         }
         MirageFairy2024.clientProxy = clientProxy
         MirageFairy2024.onClientInit.forEach {
--- src/main/kotlin/miragefairy2024/ClientProxy.kt
+++ src/main/kotlin/miragefairy2024/ClientProxy.kt
@@ -1,10 +1,12 @@
 package miragefairy2024
 
 import net.minecraft.block.Block
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.text.Text
 
 interface ClientProxy {
     fun registerItemTooltipCallback(block: (stack: ItemStack, lines: MutableList<Text>) -> Unit)
     fun registerCutoutRenderLayer(block: Block)
+    fun getClientPlayer(): PlayerEntity?
 }
