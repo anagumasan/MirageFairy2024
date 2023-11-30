4c42a8a01a6e4ff295f9d7f5e98509674455d53a
MirageFairy <idolm@ster>
2023-12-01 06:43:24 +0900

    internal: add: TextScope.empty

--- src/main/kotlin/miragefairy2024/util/TextScope.kt
+++ src/main/kotlin/miragefairy2024/util/TextScope.kt
@@ -5,6 +5,7 @@ import net.minecraft.text.Text
 inline fun text(block: TextScope.() -> Text) = block(TextScope)
 
 object TextScope {
+    fun empty(): Text = Text.empty()
     operator fun String.invoke(): Text = Text.of(this)
     fun translate(key: String): Text = Text.translatable(key)
     fun translate(key: String, vararg args: Any?): Text = Text.translatable(key, *args)
