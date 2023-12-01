ce866b9824e602a96f282b4a83840dcaff3f14ed
MirageFairy <idolm@ster>
2023-12-01 14:23:17 +0900

    internal: add: CompoundTrait

---
+++ src/main/kotlin/miragefairy2024/mod/magicplant/CompoundTrait.kt
@@ -0,0 +1,26 @@
+package miragefairy2024.mod.magicplant
+
+import net.minecraft.util.math.BlockPos
+import net.minecraft.world.World
+
+class CompoundTrait(sortKey: String, private val factor: TraitFactor, private val traitEffectKeyCard: TraitEffectKeyCard) : Trait(sortKey) {
+    override fun getTraitEffects(world: World, blockPos: BlockPos, level: Int): MutableTraitEffects? {
+        val factor = factor.getFactor(world, blockPos)
+        return if (factor != 0.0) {
+            val traitEffects = MutableTraitEffects()
+            traitEffects[traitEffectKeyCard.traitEffectKey] = 0.1 * level * factor
+            traitEffects
+        } else {
+            null
+        }
+    }
+}
+
+fun interface TraitFactor {
+    fun getFactor(world: World, blockPos: BlockPos): Double
+}
+
+fun interface TraitCondition : TraitFactor {
+    override fun getFactor(world: World, blockPos: BlockPos) = if (isValid(world, blockPos)) 1.0 else 0.0
+    fun isValid(world: World, blockPos: BlockPos): Boolean
+}
