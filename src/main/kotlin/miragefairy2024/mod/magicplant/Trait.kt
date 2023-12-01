224222429665ec8b17d594a863e6b5142911dff4
MirageFairy <idolm@ster>
2023-12-01 13:32:26 +0900

    internal: add: TraitStacks <-> NBT

--- src/main/kotlin/miragefairy2024/mod/magicplant/Trait.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/Trait.kt
@@ -1,9 +1,14 @@
 package miragefairy2024.mod.magicplant
 
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.util.string
+import miragefairy2024.util.toIdentifier
 import mirrg.kotlin.hydrogen.cmp
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder
 import net.fabricmc.fabric.api.event.registry.RegistryAttribute
+import net.minecraft.nbt.NbtCompound
+import net.minecraft.nbt.NbtElement
+import net.minecraft.nbt.NbtList
 import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKey
 import net.minecraft.util.Identifier
@@ -34,13 +39,61 @@ class TraitStack(val trait: Trait, val level: Int) {
     }
 }
 
+fun NbtCompound.toTraitStack(): TraitStack? {
+    val trait = this.getString("Trait").toIdentifier().toTrait() ?: return null
+    val level = this.getInt("Level").takeIf { it >= 1 } ?: return null
+    return TraitStack(trait, level)
+}
+
+fun TraitStack.toNbt(): NbtCompound {
+    val nbt = NbtCompound()
+    nbt.putString("Trait", this.trait.getIdentifier().string)
+    nbt.putInt("Level", this.level)
+    return nbt
+}
+
 
 // TraitStacks
 
 class TraitStacks private constructor(val traitStackMap: SortedMap<Trait, Int>) {
+    companion object {
+        fun of(traitStackList: List<TraitStack>): TraitStacks {
+            // 同じ特性をまとめて、各レベルをビットORする
+            val traitStackMap = traitStackList
+                .groupBy { it.trait }
+                .mapValues {
+                    it.value
+                        .map { traitStack -> traitStack.level }
+                        .reduce { a, b -> a or b }
+                }
+                .toSortedMap()
+            return TraitStacks(traitStackMap)
+        }
+
+        fun readFromNbt(parent: NbtCompound, key: String = "TraitStacks"): TraitStacks? {
+            if (!parent.contains(key, NbtElement.LIST_TYPE.toInt())) return null
+            return parent.getList(key, NbtElement.COMPOUND_TYPE.toInt()).toTraitStacks()
+        }
+    }
+
     init {
         traitStackMap.forEach { (_, level) ->
             require(level >= 1)
         }
     }
+}
+
+fun NbtList.toTraitStacks(): TraitStacks {
+    val traitStackList = (0..<this.size).mapNotNull {
+        this.getCompound(it).toTraitStack()
+    }
+    return TraitStacks.of(traitStackList)
+}
+
+fun TraitStacks.toNbt(): NbtList {
+    val nbt = NbtList()
+    this.traitStackMap.forEach {
+        nbt.add(TraitStack(it.key, it.value).toNbt())
+    }
+    return nbt
 }
--- src/main/kotlin/miragefairy2024/mod/magicplant/TraitUtil.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/TraitUtil.kt
@@ -1,10 +1,21 @@
 package miragefairy2024.mod.magicplant
 
 import mirrg.kotlin.hydrogen.unit
+import net.minecraft.item.ItemStack
 import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
+
+// Trait
 
 fun Trait.register(identifier: Identifier) = unit { Registry.register(traitRegistry, identifier, this) }
 
 fun Trait.getIdentifier() = traitRegistry.getId(this)!!
 fun Identifier.toTrait() = traitRegistry.get(this)
+
+
+// TraitStacks
+
+fun ItemStack.getTraitStacks(): TraitStacks? {
+    val nbt = this.nbt ?: return null
+    return TraitStacks.readFromNbt(nbt)
+}
