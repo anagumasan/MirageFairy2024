8fe5d1e2baa162ce41ba7ffe7f2a2608e59bd2a3
MirageFairy <idolm@ster>
2023-12-01 14:23:38 +0900

    internal: TraitCard

--- src/main/kotlin/miragefairy2024/mod/magicplant/TraitCard.kt
+++ src/main/kotlin/miragefairy2024/mod/magicplant/TraitCard.kt
@@ -1,12 +1,17 @@
 package miragefairy2024.mod.magicplant
 
 import miragefairy2024.MirageFairy2024
+import miragefairy2024.util.HumidityCategory
+import miragefairy2024.util.TemperatureCategory
+import miragefairy2024.util.getCrystalErg
+import miragefairy2024.util.getMoisture
+import miragefairy2024.util.humidityCategory
+import miragefairy2024.util.temperatureCategory
 import miragefairy2024.util.text
+import mirrg.kotlin.hydrogen.atLeast
 import mirrg.kotlin.hydrogen.formatAs
 import net.minecraft.util.Formatting
 import net.minecraft.util.Identifier
-import net.minecraft.util.math.BlockPos
-import net.minecraft.world.World
 
 // TraitCard
 
@@ -15,44 +20,44 @@ enum class TraitCard(
     sortKey: String,
     val enName: String,
     val jaName: String,
+    factor: TraitFactor,
+    traitEffectKeyCard: TraitEffectKeyCard,
 ) {
-    ETHER_RESPIRATION("ether_respiration", "0nutrition", "Ether Respiration", "エーテル呼吸"),
-    PHOTOSYNTHESIS("photosynthesis", "0nutrition", "Photosynthesis", "光合成"),
-    PHAEOSYNTHESIS("phaeosynthesis", "0nutrition", "Phaeosynthesis", "闇合成"),
-    OSMOTIC_ABSORPTION("osmotic_absorption", "0nutrition", "Osmotic Absorption", "浸透吸収"),
-    CRYSTAL_ABSORPTION("crystal_absorption", "0nutrition", "Crystal Absorption", "鉱物吸収"),
+    ETHER_RESPIRATION("ether_respiration", "0nutrition", "Ether Respiration", "エーテル呼吸", TraitFactors.ALWAYS, TraitEffectKeyCard.NUTRITION),
+    PHOTOSYNTHESIS("photosynthesis", "0nutrition", "Photosynthesis", "光合成", TraitFactors.LIGHT, TraitEffectKeyCard.NUTRITION),
+    PHAEOSYNTHESIS("phaeosynthesis", "0nutrition", "Phaeosynthesis", "闇合成", TraitFactors.DARKNESS, TraitEffectKeyCard.NUTRITION),
+    OSMOTIC_ABSORPTION("osmotic_absorption", "0nutrition", "Osmotic Absorption", "浸透吸収", TraitFactors.FLOOR_MOISTURE, TraitEffectKeyCard.NUTRITION),
+    CRYSTAL_ABSORPTION("crystal_absorption", "0nutrition", "Crystal Absorption", "鉱物吸収", TraitFactors.FLOOR_CRYSTAL_ERG, TraitEffectKeyCard.NUTRITION),
 
-    AIR_ADAPTATION("air_adaptation", "1atmosphere", "Air Adaptation", "空気適応"),
-    COLD_ADAPTATION("cold_adaptation", "3biome", "Cold Adaptation", "寒冷適応"),
-    WARM_ADAPTATION("warm_adaptation", "3biome", "Warm Adaptation", "温暖適応"),
-    HOT_ADAPTATION("hot_adaptation", "3biome", "Hot Adaptation", "熱帯適応"),
-    ARID_ADAPTATION("arid_adaptation", "3biome", "Arid Adaptation", "乾燥適応"),
-    MESIC_ADAPTATION("mesic_adaptation", "3biome", "Mesic Adaptation", "中湿適応"),
-    HUMID_ADAPTATION("humid_adaptation", "3biome", "Humid Adaptation", "湿潤適応"),
+    AIR_ADAPTATION("air_adaptation", "1atmosphere", "Air Adaptation", "空気適応", TraitFactors.ALWAYS, TraitEffectKeyCard.ENVIRONMENT),
+    COLD_ADAPTATION("cold_adaptation", "3biome", "Cold Adaptation", "寒冷適応", TraitFactors.LOW_TEMPERATURE, TraitEffectKeyCard.ENVIRONMENT),
+    WARM_ADAPTATION("warm_adaptation", "3biome", "Warm Adaptation", "温暖適応", TraitFactors.MEDIUM_TEMPERATURE, TraitEffectKeyCard.ENVIRONMENT),
+    HOT_ADAPTATION("hot_adaptation", "3biome", "Hot Adaptation", "熱帯適応", TraitFactors.HIGH_TEMPERATURE, TraitEffectKeyCard.ENVIRONMENT),
+    ARID_ADAPTATION("arid_adaptation", "3biome", "Arid Adaptation", "乾燥適応", TraitFactors.LOW_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
+    MESIC_ADAPTATION("mesic_adaptation", "3biome", "Mesic Adaptation", "中湿適応", TraitFactors.MEDIUM_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
+    HUMID_ADAPTATION("humid_adaptation", "3biome", "Humid Adaptation", "湿潤適応", TraitFactors.HIGH_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
 
-    SEEDS_PRODUCTION("seeds_production", "4production", "Seeds Production", "種子生成"),
-    FRUITS_PRODUCTION("fruits_production", "4production", "Fruits Production", "果実生成"),
-    LEAVES_PRODUCTION("leaves_production", "4production", "Leaves Production", "葉面生成"),
-    EXPERIENCE_PRODUCTION("experience_production", "4production", "Xp Production", "経験値生成"),
+    SEEDS_PRODUCTION("seeds_production", "4production", "Seeds Production", "種子生成", TraitFactors.ALWAYS, TraitEffectKeyCard.SEEDS_PRODUCTION),
+    FRUITS_PRODUCTION("fruits_production", "4production", "Fruits Production", "果実生成", TraitFactors.ALWAYS, TraitEffectKeyCard.FRUITS_PRODUCTION),
+    LEAVES_PRODUCTION("leaves_production", "4production", "Leaves Production", "葉面生成", TraitFactors.ALWAYS, TraitEffectKeyCard.LEAVES_PRODUCTION),
+    EXPERIENCE_PRODUCTION("experience_production", "4production", "Xp Production", "経験値生成", TraitFactors.ALWAYS, TraitEffectKeyCard.EXPERIENCE_PRODUCTION),
 
-    FAIRY_BLESSING("fairy_blessing", "5skill", "Fairy Blessing", "妖精の祝福"),
+    FAIRY_BLESSING("fairy_blessing", "5skill", "Fairy Blessing", "妖精の祝福", TraitFactors.ALWAYS, TraitEffectKeyCard.FORTUNE_FACTOR),
 
-    FOUR_LEAFED("four_leafed", "6part", "Four-leafed", "四つ葉"),
-    NODED_STEM("noded_stem", "6part", "Noded Stem", "節状の茎"),
-    FRUIT_OF_KNOWLEDGE("fruit_of_knowledge", "6part", "Fruit of Knowledge", "知識の果実"),
-    GOLDEN_APPLE("golden_apple", "6part", "Golden Apple", "金のリンゴ"),
-    SPINY_LEAVES("spiny_leaves", "6part", "Spiny Leaves", "棘状の葉"),
-    DESERT_GEM("desert_gem", "6part", "Desert Gem", "砂漠の宝石"),
-    HEATING_MECHANISM("heating_mechanism", "6part", "Heating Mechanism", "発熱機構"),
-    WATERLOGGING_TOLERANCE("waterlogging_tolerance", "6part", "Waterlogging Tolerance", "浸水耐性"),
-    ADVERSITY_FLOWER("adversity_flower", "6part", "Adversity Flower", "高嶺の花"),
-    FLESHY_LEAVES("fleshy_leaves", "6part", "Fleshy Leaves", "肉厚の葉"),
+    FOUR_LEAFED("four_leafed", "6part", "Four-leafed", "四つ葉", TraitFactors.ALWAYS, TraitEffectKeyCard.FORTUNE_FACTOR),
+    NODED_STEM("noded_stem", "6part", "Noded Stem", "節状の茎", TraitFactors.ALWAYS, TraitEffectKeyCard.GROWTH_BOOST),
+    FRUIT_OF_KNOWLEDGE("fruit_of_knowledge", "6part", "Fruit of Knowledge", "知識の果実", TraitFactors.ALWAYS, TraitEffectKeyCard.EXPERIENCE_PRODUCTION),
+    GOLDEN_APPLE("golden_apple", "6part", "Golden Apple", "金のリンゴ", TraitFactors.ALWAYS, TraitEffectKeyCard.FORTUNE_FACTOR),
+    SPINY_LEAVES("spiny_leaves", "6part", "Spiny Leaves", "棘状の葉", TraitFactors.LOW_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
+    DESERT_GEM("desert_gem", "6part", "Desert Gem", "砂漠の宝石", TraitFactors.LOW_HUMIDITY, TraitEffectKeyCard.PRODUCTION_BOOST),
+    HEATING_MECHANISM("heating_mechanism", "6part", "Heating Mechanism", "発熱機構", TraitFactors.LOW_TEMPERATURE, TraitEffectKeyCard.ENVIRONMENT),
+    WATERLOGGING_TOLERANCE("waterlogging_tolerance", "6part", "Waterlogging Tolerance", "浸水耐性", TraitFactors.HIGH_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
+    ADVERSITY_FLOWER("adversity_flower", "6part", "Adversity Flower", "高嶺の花", TraitFactors.ALWAYS, TraitEffectKeyCard.FRUITS_PRODUCTION),
+    FLESHY_LEAVES("fleshy_leaves", "6part", "Fleshy Leaves", "肉厚の葉", TraitFactors.LOW_HUMIDITY, TraitEffectKeyCard.ENVIRONMENT),
     ;
 
     val identifier = Identifier(MirageFairy2024.modId, path)
-    val trait = object : Trait(sortKey) {
-        override fun getTraitEffects(world: World, blockPos: BlockPos, level: Int) = null // TODO
-    }
+    val trait = CompoundTrait(sortKey, factor, traitEffectKeyCard)
 }
 
 
@@ -80,4 +85,21 @@ enum class TraitEffectKeyCard(
         override fun getDescription(value: Double) = text { getName() + (value * 100 formatAs "%+.0f%%")() }
         override fun getDefaultValue() = 0.0
     }
+}
+
+
+// TraitFactor
+
+object TraitFactors {
+    val ALWAYS = TraitFactor { _, _ -> 1.0 }
+    val FLOOR_MOISTURE = TraitFactor { world, blockPos -> world.getMoisture(blockPos.down()) }
+    val FLOOR_CRYSTAL_ERG = TraitFactor { world, blockPos -> world.getCrystalErg(blockPos.down()) }
+    val LIGHT = TraitFactor { world, blockPos -> (world.getLightLevel(blockPos) - 8 atLeast 0) / 7.0 }
+    val DARKNESS = TraitFactor { world, blockPos -> ((15 - world.getLightLevel(blockPos)) - 8 atLeast 0) / 7.0 }
+    val LOW_TEMPERATURE = TraitCondition { world, blockPos -> world.getBiome(blockPos).temperatureCategory == TemperatureCategory.LOW }
+    val MEDIUM_TEMPERATURE = TraitCondition { world, blockPos -> world.getBiome(blockPos).temperatureCategory == TemperatureCategory.MEDIUM }
+    val HIGH_TEMPERATURE = TraitCondition { world, blockPos -> world.getBiome(blockPos).temperatureCategory == TemperatureCategory.HIGH }
+    val LOW_HUMIDITY = TraitCondition { world, blockPos -> world.getBiome(blockPos).humidityCategory == HumidityCategory.LOW }
+    val MEDIUM_HUMIDITY = TraitCondition { world, blockPos -> world.getBiome(blockPos).humidityCategory == HumidityCategory.MEDIUM }
+    val HIGH_HUMIDITY = TraitCondition { world, blockPos -> world.getBiome(blockPos).humidityCategory == HumidityCategory.HIGH }
 }
