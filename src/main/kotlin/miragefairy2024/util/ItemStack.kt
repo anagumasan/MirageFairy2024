673299a27b7cf8c576aa65e53201c6720bc9386d
MirageFairy <idolm@ster>
2023-12-01 15:27:24 +0900

    internal: createItemStack

--- src/main/kotlin/miragefairy2024/util/ItemStack.kt
+++ src/main/kotlin/miragefairy2024/util/ItemStack.kt
@@ -1,8 +1,9 @@
 package miragefairy2024.util
 
+import mirrg.kotlin.hydrogen.atMost
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
 
-fun Item.createItemStack(count: Int = 1) = ItemStack(this, count)
+fun Item.createItemStack(count: Int = 1) = ItemStack(this, count atMost this.maxCount)
 
 val EMPTY_ITEM_STACK: ItemStack get() = ItemStack.EMPTY
