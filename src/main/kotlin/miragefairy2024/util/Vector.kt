75d77d95fcef61de74dcaa258242e491cd503e02
MirageFairy <idolm@ster>
2023-12-02 15:02:53 +0900

    internal: add: BlockPos.toBox

--- src/main/kotlin/miragefairy2024/util/Vector.kt
+++ src/main/kotlin/miragefairy2024/util/Vector.kt
@@ -1,6 +1,9 @@
 package miragefairy2024.util
 
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
 import net.minecraft.util.math.Vec3d
 
 fun Vec3d.toBlockPos(): BlockPos = BlockPos.ofFloored(this)
+
+fun BlockPos.toBox() = Box(this)
