d3aeaa2ceb0b83375910a23126e0c9d09f73f78d
MirageFairy <idolm@ster>
2023-12-09 18:00:22 +0900

    internal: ItemStack: add

--- src/main/kotlin/miragefairy2024/util/ItemStack.kt
+++ src/main/kotlin/miragefairy2024/util/ItemStack.kt
@@ -3,7 +3,12 @@ package miragefairy2024.util
 import mirrg.kotlin.hydrogen.atMost
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.nbt.NbtCompound
 
 fun Item.createItemStack(count: Int = 1) = ItemStack(this, count atMost this.maxCount)
 
 val EMPTY_ITEM_STACK: ItemStack get() = ItemStack.EMPTY
+
+fun ItemStack.toNbt(): NbtCompound = NbtCompound().also { this.writeNbt(it) }
+
+fun NbtCompound.toItemStack(): ItemStack = ItemStack.fromNbt(this)
